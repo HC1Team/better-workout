cf91d18acde8d8bf0792fb7a494f218ff9c8a91c
Jordan Glover <jordanglover107@gmail.com>
2020-11-06 23:56:52 -0500

    beginnings of timer page

--- frontend/better-workout-timer/src/Components/Timer.js
+++ frontend/better-workout-timer/src/Components/Timer.js
@@ -35,11 +35,43 @@ class Timer extends Component {
     
   //   this.setState({ responseToPost: body });
   // };
-  
+
+  //Review code from original project to try to make this work, but make it your own
+  //Draws the circle with the time or other details in middle
+  draw() {
+    //Draw circle
+    var canvas = document.getElementById('timerCircle');
+    var ctx = canvas.getContext("2d"); 
+    var X = canvas.width / 2;
+    var Y = canvas.height / 2;
+    var R = 220;
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    ctx.lineWidth = 25;
+    ctx.strokeStyle = '#73DD5E';
+    ctx.stroke(); 
+  }
+
+  //Redraw the circle (or draw something over to make it apear to disappear) every second
+
+
+  componentDidMount() {
+    this.draw();
+  }
+
 render() {
     return (
       <div className="App">
-        <h1>Timer</h1>
+        {/* <h1>Timer</h1> */}
+        <div className="timer-info-container">
+            <p className="timer-info-item1 timer-info-top-item">Rounds Remaining</p>
+            <p className="timer-info-item2 timer-info-top-item">Current Exercise</p>
+            <p className="timer-info-item3 timer-info-top-item">Next Up</p>
+            <p className="timer-info-item4 timer-info-bottom-item" id="rounds-remaining">3</p>
+            <p className="timer-info-item5 timer-info-bottom-item" id="current-exercise">push-ups</p>
+            <p className="timer-info-item6 timer-info-bottom-item" id="next-up">jumping jacks</p>
+        </div>
+        <canvas id="timerCircle" width="500" height="500"></canvas>
       </div>
     );
   }
