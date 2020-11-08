7709e98f92f6a1c8c3f474c3b8f1d09e406a0683
Jordan Glover <jordanglover107@gmail.com>
2020-11-07 19:50:54 -0500

    started creating circle for timer

--- frontend/better-workout-timer/src/Components/Timer.js
+++ frontend/better-workout-timer/src/Components/Timer.js
@@ -1,63 +1,36 @@
 import React, {Component} from 'react';
 import './Styles/App.css';
+import './Styles/timer.css'
 
 class Timer extends Component {
-  // state = {
-  //   response: '',
-  //   post: '',
-  //   responseToPost: '',
-  // };
-  
-  // componentDidMount() {
-  //   this.callApi()
-  //     .then(res => this.setState({ response: res.express }))
-  //     .catch(err => console.log(err));
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {roundsCompleted: 0};
   // }
-  
-  // callApi = async () => {
-  //   const response = await fetch('/api/hello');
-  //   const body = await response.json();
-  //   if (response.status !== 200) throw Error(body.message);
-    
-  //   return body;
-  // };
-  
-  // handleSubmit = async e => {
-  //   e.preventDefault();
-  //   const response = await fetch('/api/world', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({ post: this.state.post }),
-  //   });
-  //   const body = await response.text();
-    
-  //   this.setState({ responseToPost: body });
-  // };
 
-  //Review code from original project to try to make this work, but make it your own
-  //Draws the circle with the time or other details in middle
-  draw() {
-    //Draw circle
-    var canvas = document.getElementById('timerCircle');
-    var ctx = canvas.getContext("2d"); 
-    var X = canvas.width / 2;
-    var Y = canvas.height / 2;
-    var R = 220;
-    ctx.beginPath();
-    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
-    ctx.lineWidth = 25;
-    ctx.strokeStyle = '#73DD5E';
-    ctx.stroke(); 
-  }
 
   //Redraw the circle (or draw something over to make it apear to disappear) every second
 
 
-  componentDidMount() {
-    this.draw();
-  }
+  // componentDidMount() {
+    
+  // }
+
+  formatTimeLeft(time) {
+    // The largest round integer less than or equal to the result of time divided being by 60.
+    const minutes = Math.floor(time / 60);
+
+    // Seconds are the remainder of the time divided by 60 (modulus operator)
+    let seconds = time % 60;
+
+    // If the value of seconds is less than 10, then display seconds with a leading zero
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+
+    // The output in MM:SS format
+    return `${minutes}:${seconds}`;
+    }
 
 render() {
     return (
@@ -71,7 +44,17 @@ render() {
             <p className="timer-info-item5 timer-info-bottom-item" id="current-exercise">push-ups</p>
             <p className="timer-info-item6 timer-info-bottom-item" id="next-up">jumping jacks</p>
         </div>
-        <canvas id="timerCircle" width="500" height="500"></canvas>
+
+        <div className="base-timer">
+          <svg className="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+            <g className="base-timer__circle">
+              <circle className="base-timer__path-elapsed" cx="50" cy="50" r="45"/>
+            </g>
+          </svg>
+          <span id="time-remaining">
+            {/* {this.formatTimeLeft(timeLeft)} */}
+          </span>
+        </div>
       </div>
     );
   }
