83ae3f07258987ec3fb9b5479c8e66a17f1353ac
Jordan Glover <jordanglover107@gmail.com>
2020-11-25 23:01:52 -0500

    started building skeleton for cards

--- frontend/better-workout-timer/src/Components/SavedWorkouts.js
+++ frontend/better-workout-timer/src/Components/SavedWorkouts.js
@@ -1,7 +1,69 @@
-import React, {Component} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import './Styles/App.css';
+import './Styles/savedWorkouts.css';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+const axios = require('axios');
 
 export default function SavedWorkouts() {
+  var sampleWorkouts = [
+    { "workoutID": "Workout1",
+      "numberOfRounds": 3,
+      "exercises":
+        [
+          {"exerciseName":"Pull ups", "muscleGroup": "Upper Body", "repTarget": 10, "time": 60},
+          {"exerciseName":"Burpees", "muscleGroup": "Full Body", "repTarget": 10, "time": 60},
+          {"exerciseName":"Squats", "muscleGroup": "Lower Body", "repTarget": 10, "time": 60},
+          {"exerciseName":"Jump Rope", "muscleGroup": "Full Body", "repTarget": 0, "time": 60}
+        ]
+    },
+    { "workoutID": "Workout2",
+      "numberOfRounds": 3,
+      "exercises":
+        [
+          {"exerciseName":"Pulsing Squats", "muscleGroup": "Lower Body", "repTarget": 10, "time": 30},
+          {"exerciseName":"Burpees", "muscleGroup": "Full Body", "repTarget": 10, "time": 60},
+          {"exerciseName":"Supermans", "muscleGroup": "Abs", "repTarget": 0, "time": 60},
+          {"exerciseName":"Pull ups", "muscleGroup": "Upper body", "repTarget": 0, "time": 45}
+        ]
+    },
+    { "workoutID": "Workout3",
+      "numberOfRounds": 3,
+      "exercises":
+        [
+          {"exerciseName":"Push-Ups", "muscleGroup": "Upper Body", "repTarget": 10, "time": 30},
+          {"exerciseName":"Burpees", "muscleGroup": "Upper Body", "repTarget": 10, "time": 60},
+          {"exerciseName":"Squats", "muscleGroup": "Lower Body", "repTarget": 10, "time": 60},
+          {"exerciseName":"Plank", "muscleGroup": "Abs", "repTarget": 0, "time": 45}
+        ]
+    },
+  ]
+
+  const [workouts, setWorkouts] = useState([]);
+
+  // useEffect(() => {
+  //   //Set up initial stuff
+  //   getWorkouts();
+  //   return () => {
+  //     //clean stuff up if needed
+  //   }
+  // });
+
+  //Get workouts from database
+  //HAS NOT BEEN TESTED. Just skeleton code.
+  function getWorkouts() {
+    axios.get('http://localhost:8000/workouts')
+        .then((response) => {
+          console.log(response.data);
+          setWorkouts(response.data);
+          console.log(workouts);
+        }, (error) => {
+
+        });
+  }
+
+
   // state = {
   //   response: '',
   //   post: '',
@@ -36,10 +98,41 @@ export default function SavedWorkouts() {
   //   this.setState({ responseToPost: body });
   // };
   
-
+    //TODO: Test how it looks like!
     return (
       <div className="App">
         <h1>Saved Routines</h1>
+        {/* Container for your cards */}
+        <div className="large-container">
+          {/* Each card will be mapped out here, but for now you'll just make some temp one's until you have what you need */}
+          {sampleWorkouts.map( (w ,idx) => {
+            <Card 
+              key={idx}
+              className="workout-card"
+            >
+              <Card.Body
+                key={e+idx}
+              >
+                <Card.Title id={w.workoutID} className="workout-title">{w.workoutID}</Card.Title>
+                <ListGroup variant="flush">
+                  {w.exercises.map((e, idx) => {
+                    <ListGroup.Item className="exercise-info-container">
+                      <p className="exercise-info-item1 exercise-info-top">Exercise:</p>
+                      <p className="exercise-info-item2 exercise-info-top">Muscle Group:</p>
+                      <p className="exercise-info-item3 exercise-info-top">Rep Target:</p>
+                      <p className="exercise-info-item4 exercise-info-top">Time:</p>
+                  <p className="exercise-info-item5 exercise-info-bottom">{e.exerciseName}</p>
+                  <p className="exercise-info-item6 exercise-info-bottom">{e.muscleGroup}</p>
+                  <p className="exercise-info-item7 exercise-info-bottom">{e.repTarget}</p>
+                  <p className="exercise-info-item8 exercise-info-bottom">{e.time}</p>
+                    </ListGroup.Item>
+                    })}
+                </ListGroup>
+              </Card.Body>
+            </Card>
+          })}
+          
+        </div>
       </div>
     );
 }
