a8872d87bb197a0c729be1ebda5ccf390d687536
Gard Zeringue <46787912+gardmz@users.noreply.github.com>
2020-11-27 16:48:37 -0500

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
+// Create express app
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const cors = require("cors");
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
+//Routes - seperated routes and server
+app.use(require('./routes')); 
 
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-  // Figure out how to send html instead?
-  // Should be fun
-});
+// Server port
+let HTTP_PORT = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+let server = app.listen(HTTP_PORT, function () {
+
+    let host = server.address().address
+    let port = server.address().port
+  
+    console.log("App listening at local", host, port)
+})
